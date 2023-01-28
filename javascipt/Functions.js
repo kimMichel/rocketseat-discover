3904e0a7369f9b936164eff42799bebb82b37cf1
kim <kim@xds.com.br>
2023-01-28 15:03:18 -0300

    functions

---
+++ javascipt/Functions.js
@@ -0,0 +1,23 @@
+function sum(x, y) {
+    return x + y
+}
+
+function minus(x, y) {
+    return x - y
+}
+
+function divide(x, y) {
+    return x / y
+}
+
+function times(x, y) {
+    return x * y
+}
+ 
+let a = 5
+let b = 6
+
+console.log(`A soma dos numeros ${a} e ${b} da: ${sum(a, b)}`)
+console.log(`A subtração dos numeros ${a} e ${b} da: ${minus(a, b)}`)
+console.log(`A divisão dos numeros ${a} e ${b} da: ${divide(a, b)}`)
+console.log(`A multiplicação dos numeros ${a} e ${b} da: ${times(a, b)}`)
